3013b5c94bddf063ae1617c2080ee6324613a4af
shahdkh23-meet <109900082+shahdkh23-meet@users.noreply.github.com>
2022-08-03 10:32:02 +0300

    11

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -2,15 +2,39 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+
+app = Flask(  
+    __name__,
+    template_folder='templates',  
+    static_folder='static'  
+
+)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = python3auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('index_tweet'))
+        except:
+            error = "Authentication failed"
+    return render_template("signin.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def design():
+    return render_template("index.html")
 
 
 
 
-#Code goes above here
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
